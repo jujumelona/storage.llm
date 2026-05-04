8900ca1a2d022cd4a5a4200d9bd2343a4ef3ac1c
jujju <jujju@gmail.com>
2026-05-05 00:11:08 +0900

    Order paged KV type include before engine state

--- moe_engine/src/moe_pc_engine.cpp
+++ moe_engine/src/moe_pc_engine.cpp
@@ -84,8 +84,8 @@ static inline __m256 moe_madd_ps(__m256 a, __m256 b, __m256 c) {
 // Solution: Keep only second include, remove first (types are guarded anyway)
 // Impact: Eliminates duplicate static function compilation, reduces binary size
 #include "parts/generation_batch_scheduler.cpp.inc"
-#include "parts/engine_state.cpp.inc"
 #include "parts/generation_paged_kv.cpp.inc"
+#include "parts/engine_state.cpp.inc"
 #include "parts/csv_numbers.cpp.inc"
 #include "parts/projection_parse.cpp.inc"
 #include "parts/tensor_slots.cpp.inc"
